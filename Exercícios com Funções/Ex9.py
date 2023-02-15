1623807a6fd037a864344f08e8985ab7be745a0a
rriqueti <57726626+rriqueti@users.noreply.github.com>
2023-02-14 21:19:28 -0300

    Create Ex9.py

---
+++ Exercícios com Funções/Ex9.py
@@ -0,0 +1,9 @@
+#Reverso do número. Faça uma função que retorne o reverso de um  número inteiro informado. Por exemplo: 127 -> 721.
+
+def reverso(numero):
+    return print(numero[::-1])
+
+reverso('Reverter palavras')
+
+
+#'sarvalap retreveR'
